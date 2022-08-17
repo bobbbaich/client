83537f2fb9a4f086760e2ad685d8c95835354ea8
bohdanpohorilyi <bohdan.pohorilyi@base.ai>
2022-08-17 17:37:50 +0300

    update: invalidate distribution

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload. <br/>
-          Update check cache.
+          Update check cache - invalidated by pipeline.
         </p>
         <a
           className="App-link"
