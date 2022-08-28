34277d88a5c7d7f5a022d9f70e0f3e64fc0d8381
bohdanpohorilyi <bohdan.pohorilyi@base.ai>
2022-08-28 21:18:29 +0300

    test invalidate

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,27 @@ import Instructions from './Instructions.js'
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. <br/>
-          Update check cache - invalidated by pipeline in the pool.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Instructions />
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload. <br/>
+                    Update check cache - invalidated by pipeline in the pool.
+                </p>
+                <p>Cache has been invalidated</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <Instructions/>
+        </div>
+    );
 }
 
 export default App;
